34177bec337d6e3f2268ff8719c7dd334fdc70ee
vizzon <fvizzon@hipcam.com>
2023-06-25 13:40:55 -0300

    TS6 rc1

--- TS6/untitled0.py
+++ TS6/untitled0.py
@@ -5,7 +5,6 @@ Created on Sat Jun 24 11:19:20 2023
 
 @author: vizzon
 """
-
 import numpy as np
 from IPython.display import display, Markdown
 import matplotlib.pyplot as plt
@@ -16,16 +15,12 @@ dev = False
 
 wo = 300*2*np.pi
 wo_n = 1
-Q = 1/np.sqrt(2)
-a = 1/3
+Q = 1
+a = 1/9
 c = 1
 num_lp = np.array([a, 0, c])
-den_lp = np.array([1, wo_n/Q, wo_n**2])
-#analyze_sys(signal.TransferFunction(num_lp, den_lp))
-num_hp = np.array([c, 0, a])
-den_hp = np.array([1, wo_n/Q, wo_n**2])
-#analyze_sys(signal.TransferFunction(num_hp, den_hp))
-
-num_hp_2 = np.array([c, 0, a*(wo**2)])
-den_hp_2 = np.array([1, wo/Q, wo**2])
-analyze_sys(signal.TransferFunction(num_hp_2, den_hp_2))
+den_lp = np.array([1, 2, 2, 1])
+analyze_sys([signal.TransferFunction(num_lp, den_lp)])
+num_hp = np.array([c, 0, a, 0])
+den_hp = np.array([1, 2, 2, 1])
+analyze_sys([signal.TransferFunction(num_hp, den_hp)])
